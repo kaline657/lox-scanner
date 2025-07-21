4bb2748fb636951d309e03cc969c5ea46ced44c0
Kaline Maria Carvalho <carvalho.kaline@discente.ufma.br>
2025-07-20 23:39:57 -0300

    feat: suporte a literais e agrupamentos

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -4,9 +4,7 @@ class Interpreter implements Expr.Visitor<Object> {
 
     Object interpret(Expr expression) {
         try {
-            Object value = evaluate(expression);
-            System.out.println(stringify(value));
-            return value;
+            return evaluate(expression);
         } catch (RuntimeError error) {
             Lox.runtimeError(error);
             return null;
@@ -26,54 +24,3 @@ class Interpreter implements Expr.Visitor<Object> {
     public Object visitGroupingExpr(Expr.Grouping expr) {
         return evaluate(expr.expression);
     }
-
-    @Override
-    public Object visitUnaryExpr(Expr.Unary expr) {
-        Object right = evaluate(expr.right);
-        switch (expr.operator.type) {
-            case MINUS:
-                return -(double) right;
-            case BANG:
-                return !isTruthy(right);
-        }
-        return null;
-    }
-
-    @Override
-    public Object visitBinaryExpr(Expr.Binary expr) {
-        Object left = evaluate(expr.left);
-        Object right = evaluate(expr.right);
-        switch (expr.operator.type) {
-            case PLUS:
-                if (left instanceof Double && right instanceof Double) {
-                    return (double) left + (double) right;
-                }
-                return stringify(left) + stringify(right);
-            case MINUS:
-                return (double) left - (double) right;
-            case STAR:
-                return (double) left * (double) right;
-            case SLASH:
-                return (double) left / (double) right;
-        }
-        return null;
-    }
-
-    private boolean isTruthy(Object object) {
-        if (object == null) return false;
-        if (object instanceof Boolean) return (boolean) object;
-        return true;
-    }
-
-    private String stringify(Object object) {
-        if (object == null) return "nil";
-        if (object instanceof Double) {
-            String text = object.toString();
-            if (text.endsWith(".0")) {
-                text = text.substring(0, text.length() - 2);
-            }
-            return text;
-        }
-        return object.toString();
-    }
-}
