f8c360b0c730ffecd63f1db8a7f88e2a2f0de77c
Kaline Maria Carvalho <carvalho.kaline@discente.ufma.br>
2025-07-20 23:44:25 -0300

    feat: avaliação de expressões binárias (aritméticas e lógicas)

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -43,3 +43,47 @@ class Interpreter implements Expr.Visitor<Object> {
         if (object instanceof Boolean) return (boolean) object;
         return true;
     }
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case BANG_EQUAL:
+                return !isEqual(left, right);
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+            case GREATER:
+                return (double) left > (double) right;
+            case GREATER_EQUAL:
+                return (double) left >= (double) right;
+            case LESS:
+                return (double) left < (double) right;
+            case LESS_EQUAL:
+                return (double) left <= (double) right;
+            case MINUS:
+                return (double) left - (double) right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double) left + (double) right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    return (String) left + (String) right;
+                }
+
+                throw new RuntimeError(expr.operator, "Operandos devem ser dois números ou duas strings.");
+            case SLASH:
+                return (double) left / (double) right;
+            case STAR:
+                return (double) left * (double) right;
+        }
+
+        return null;
+    }
+
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null) return true;
+        if (a == null) return false;
+        return a.equals(b);
+    }
