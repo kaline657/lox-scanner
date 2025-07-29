6beeae1fc93b212fda8b45eae638091810859986
Kaline Maria Carvalho <carvalho.kaline@discente.ufma.br>
2025-07-28 23:12:10 -0300

    feat: parser completo com suporte a funções, return e chamadas

--- Parser.java
+++ Parser.java
@@ -1,171 +1,329 @@
 package br.ufma.ecp.jlox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static br.ufma.ecp.jlox.TokenType.*;
 
 class Parser {
-  private static class ParseError extends RuntimeException {}
 
-  private final List<Token> tokens;
-  private int current = 0;
+    private static class ParseError extends RuntimeException {}
 
-  Parser(List<Token> tokens) {
-    this.tokens = tokens;
-  }
+    private final List<Token> tokens;
+    private int current = 0;
 
-  Expr parse() {
-    try {
-      return expression();
-    } catch (ParseError error) {
-      return null;
-    }
-  }
-
-  private Expr expression() {
-    return equality();
-  }
-
-  private Expr equality() {
-    Expr expr = comparison();
-
-    while (match(BANG_EQUAL, EQUAL_EQUAL)) {
-      Token operator = previous();
-      Expr right = comparison();
-      expr = new Expr.Binary(expr, operator, right);
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
     }
 
-    return expr;
-  }
-
-  private Expr comparison() {
-    Expr expr = term();
-
-    while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
-      Token operator = previous();
-      Expr right = term();
-      expr = new Expr.Binary(expr, operator, right);
+    Expr parse() {
+        try {
+            return expression();
+        } catch (ParseError error) {
+            return null;
+        }
     }
 
-    return expr;
-  }
-
-  private Expr term() {
-    Expr expr = factor();
-
-    while (match(MINUS, PLUS)) {
-      Token operator = previous();
-      Expr right = factor();
-      expr = new Expr.Binary(expr, operator, right);
+    List<Stmt> parseStatements() {
+        List<Stmt> statements = new ArrayList<>();
+        while (!isAtEnd()) {
+            statements.add(declaration());
+        }
+        return statements;
     }
 
-    return expr;
-  }
-
-  private Expr factor() {
-    Expr expr = unary();
-
-    while (match(SLASH, STAR)) {
-      Token operator = previous();
-      Expr right = unary();
-      expr = new Expr.Binary(expr, operator, right);
+    private Stmt declaration() {
+        try {
+            if (match(FUN)) return function("função");
+            if (match(VAR)) return varDeclaration();
+            return statement();
+        } catch (ParseError error) {
+            synchronize();
+            return null;
+        }
     }
 
-    return expr;
-  }
+    private Stmt varDeclaration() {
+        Token name = consume(IDENTIFIER, "Esperado nome da variável.");
 
-  private Expr unary() {
-    if (match(BANG, MINUS)) {
-      Token operator = previous();
-      Expr right = unary();
-      return new Expr.Unary(operator, right);
+        Expr initializer = null;
+        if (match(EQUAL)) {
+            initializer = expression();
+        }
+
+        consume(SEMICOLON, "Esperado ';' após declaração.");
+        return new Stmt.Var(name, initializer);
     }
 
-    return primary();
-  }
-
-  private Expr primary() {
-    if (match(FALSE)) return new Expr.Literal(false);
-    if (match(TRUE)) return new Expr.Literal(true);
-    if (match(NIL)) return new Expr.Literal(null);
-
-    if (match(NUMBER, STRING)) {
-      return new Expr.Literal(previous().literal);
+    private Stmt function(String tipo) {
+        Token name = consume(IDENTIFIER, "Esperado nome da " + tipo + ".");
+        consume(LEFT_PAREN, "Esperado '(' após o nome da função.");
+        List<Token> parameters = new ArrayList<>();
+        if (!check(RIGHT_PAREN)) {
+            do {
+                if (parameters.size() >= 255) {
+                    error(peek(), "Máximo de 255 parâmetros permitido.");
+                }
+                parameters.add(consume(IDENTIFIER, "Esperado nome do parâmetro."));
+            } while (match(COMMA));
+        }
+        consume(RIGHT_PAREN, "Esperado ')' após parâmetros.");
+        consume(LEFT_BRACE, "Esperado '{' antes do corpo da função.");
+        List<Stmt> body = block();
+        return new Stmt.Function(name, parameters, body);
     }
 
-    if (match(LEFT_PAREN)) {
-      Expr expr = expression();
-      consume(RIGHT_PAREN, "Esperava ')' após a expressão.");
-      return new Expr.Grouping(expr);
+    private Stmt statement() {
+        if (match(RETURN)) return returnStatement();
+        if (match(IF)) return ifStatement();
+        if (match(PRINT)) return printStatement();
+        if (match(LEFT_BRACE)) return new Stmt.Block(block());
+
+        return expressionStatement();
     }
 
-    throw error(peek(), "Esperava uma expressão.");
-  }
+    private Stmt returnStatement() {
+        Token keyword = previous();
+        Expr value = null;
+        if (!check(SEMICOLON)) {
+            value = expression();
+        }
 
-  private boolean match(TokenType... types) {
-    for (TokenType type : types) {
-      if (check(type)) {
+        consume(SEMICOLON, "Esperado ';' após return.");
+        return new Stmt.Return(keyword, value);
+    }
+
+    private Stmt ifStatement() {
+        consume(LEFT_PAREN, "Esperado '(' após 'if'.");
+        Expr condition = expression();
+        consume(RIGHT_PAREN, "Esperado ')' após condição.");
+
+        Stmt thenBranch = statement();
+        Stmt elseBranch = null;
+        if (match(ELSE)) {
+            elseBranch = statement();
+        }
+
+        return new Stmt.If(condition, thenBranch, elseBranch);
+    }
+
+    private Stmt printStatement() {
+        Expr value = expression();
+        consume(SEMICOLON, "Esperado ';' após valor.");
+        return new Stmt.Print(value);
+    }
+
+    private Stmt expressionStatement() {
+        Expr expr = expression();
+        consume(SEMICOLON, "Esperado ';' após expressão.");
+        return new Stmt.Expression(expr);
+    }
+
+    private List<Stmt> block() {
+        List<Stmt> statements = new ArrayList<>();
+
+        while (!check(RIGHT_BRACE) && !isAtEnd()) {
+            statements.add(declaration());
+        }
+
+        consume(RIGHT_BRACE, "Esperado '}' após bloco.");
+        return statements;
+    }
+
+    private Expr expression() {
+        return assignment();
+    }
+
+    private Expr assignment() {
+        Expr expr = equality();
+
+        if (match(EQUAL)) {
+            Token equals = previous();
+            Expr value = assignment();
+
+            if (expr instanceof Expr.Variable) {
+                Token name = ((Expr.Variable) expr).name;
+                return new Expr.Assign(name, value);
+            }
+
+            error(equals, "Atribuição inválida.");
+        }
+
+        return expr;
+    }
+
+    private Expr equality() {
+        Expr expr = comparison();
+
+        while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr comparison() {
+        Expr expr = term();
+
+        while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+            Token operator = previous();
+            Expr right = term();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr term() {
+        Expr expr = factor();
+
+        while (match(MINUS, PLUS)) {
+            Token operator = previous();
+            Expr right = factor();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr factor() {
+        Expr expr = unary();
+
+        while (match(SLASH, STAR)) {
+            Token operator = previous();
+            Expr right = unary();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr unary() {
+        if (match(BANG, MINUS)) {
+            Token operator = previous();
+            Expr right = unary();
+            return new Expr.Unary(operator, right);
+        }
+
+        return call();
+    }
+
+    private Expr call() {
+        Expr expr = primary();
+
+        while (true) {
+            if (match(LEFT_PAREN)) {
+                expr = finishCall(expr);
+            } else {
+                break;
+            }
+        }
+
+        return expr;
+    }
+
+    private Expr finishCall(Expr callee) {
+        List<Expr> arguments = new ArrayList<>();
+        if (!check(RIGHT_PAREN)) {
+            do {
+                if (arguments.size() >= 255) {
+                    error(peek(), "Máximo de 255 argumentos.");
+                }
+                arguments.add(expression());
+            } while (match(COMMA));
+        }
+
+        Token paren = consume(RIGHT_PAREN, "Esperado ')' após argumentos.");
+
+        return new Expr.Call(callee, paren, arguments);
+    }
+
+    private Expr primary() {
+        if (match(FALSE)) return new Expr.Literal(false);
+        if (match(TRUE)) return new Expr.Literal(true);
+        if (match(NIL)) return new Expr.Literal(null);
+
+        if (match(NUMBER, STRING)) {
+            return new Expr.Literal(previous().literal);
+        }
+
+        if (match(IDENTIFIER)) {
+            return new Expr.Variable(previous());
+        }
+
+        if (match(LEFT_PAREN)) {
+            Expr expr = expression();
+            consume(RIGHT_PAREN, "Esperado ')' após expressão.");
+            return new Expr.Grouping(expr);
+        }
+
+        throw error(peek(), "Esperado expressão.");
+    }
+
+    private boolean match(TokenType... types) {
+        for (TokenType type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean check(TokenType type) {
+        if (isAtEnd()) return false;
+        return peek().type == type;
+    }
+
+    private Token advance() {
+        if (!isAtEnd()) current++;
+        return previous();
+    }
+
+    private boolean isAtEnd() {
+        return peek().type == EOF;
+    }
+
+    private Token peek() {
+        return tokens.get(current);
+    }
+
+    private Token previous() {
+        return tokens.get(current - 1);
+    }
+
+    private Token consume(TokenType type, String message) {
+        if (check(type)) return advance();
+        throw error(peek(), message);
+    }
+
+    private ParseError error(Token token, String message) {
+        Lox.error(token, message);
+        return new ParseError();
+    }
+
+    private void synchronize() {
         advance();
-        return true;
-      }
+
+        while (!isAtEnd()) {
+            if (previous().type == SEMICOLON) return;
+
+            switch (peek().type) {
+                case CLASS:
+                case FUN:
+                case VAR:
+                case FOR:
+                case IF:
+                case WHILE:
+                case PRINT:
+                case RETURN:
+                    return;
+            }
+
+            advance();
+        }
     }
-    return false;
-  }
-
-  private Token consume(TokenType type, String message) {
-    if (check(type)) return advance();
-    throw error(peek(), message);
-  }
-
-  private boolean check(TokenType type) {
-    if (isAtEnd()) return false;
-    return peek().type == type;
-  }
-
-  private Token advance() {
-    if (!isAtEnd()) current++;
-    return previous();
-  }
-
-  private boolean isAtEnd() {
-    return peek().type == EOF;
-  }
-
-  private Token peek() {
-    return tokens.get(current);
-  }
-
-  private Token previous() {
-    return tokens.get(current - 1);
-  }
-
-  private ParseError error(Token token, String message) {
-    Lox.error(token, message);
-    return new ParseError();
-  }
-
-  private void synchronize() {
-    advance();
-    while (!isAtEnd()) {
-      if (previous().type == SEMICOLON) return;
-
-      switch (peek().type) {
-        case CLASS:
-        case FUN:
-        case VAR:
-        case FOR:
-        case IF:
-        case WHILE:
-        case PRINT:
-        case RETURN:
-          return;
-      }
-
-      advance();
-    }
-  }
 }
-
-
-
 
